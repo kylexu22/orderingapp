90ffa7a6066f82f91fc7492f8d7dd272172a2a27
Kyle <kylexu22@gmail.com>
2026-02-25 02:03:47 -0500

    printer x5

--- next.config.js
+++ next.config.js
@@ -1,10 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["@sparticuz/chromium", "puppeteer-core"]
+  },
   outputFileTracingIncludes: {
     "/api/cloudprnt": [
+      "node_modules/@sparticuz/chromium/**",
+      "node_modules/puppeteer-core/**",
       "./node_modules/@sparticuz/chromium/bin/**"
     ],
     "/api/cloudprnt/route": [
+      "node_modules/@sparticuz/chromium/**",
+      "node_modules/puppeteer-core/**",
+      "./node_modules/@sparticuz/chromium/bin/**"
+    ],
+    "/app/api/cloudprnt/route": [
+      "node_modules/@sparticuz/chromium/**",
+      "node_modules/puppeteer-core/**",
       "./node_modules/@sparticuz/chromium/bin/**"
     ]
   }
