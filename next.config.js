609899a5e49b64f9e79aaa0ae7b56db83b9a5c43
Kyle <kylexu22@gmail.com>
2026-02-25 01:59:44 -0500

    receipt x4

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  outputFileTracingIncludes: {
+    "/api/cloudprnt": [
+      "./node_modules/@sparticuz/chromium/bin/**"
+    ],
+    "/api/cloudprnt/route": [
+      "./node_modules/@sparticuz/chromium/bin/**"
+    ]
+  }
+};
 
 module.exports = nextConfig;
